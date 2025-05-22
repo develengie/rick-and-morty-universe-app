9d7f48a9f25f60e362472da5ab57a81109f9da4d
Alexey <alexeyushakov1997@yandex.ru>
2025-05-23 02:04:43 +0300

    feat: created ElementDetailPage

---
+++ src/app/components/common/tables/ElementDetailTable.tsx
@@ -0,0 +1,43 @@
+import type {
+    CharactersData,
+    LocationData,
+    EpisodeData,
+} from '../../../models/models';
+
+type Element = {
+    element: CharactersData | LocationData | EpisodeData;
+};
+
+const ElementDetailTable = ({ element }: Element) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    {Object.keys(element).map(item => (
+                        <th key={item}>
+                            {item === 'id'
+                                ? item.toUpperCase()
+                                : item.charAt(0).toUpperCase() + item.slice(1)}
+                        </th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    {Object.values(element).map(item => (
+                        <td key={item}>
+                            {typeof item === 'string' &&
+                            item.startsWith('https://') ? (
+                                <img src={item} alt="" />
+                            ) : (
+                                <>{item ? item : '-'}</>
+                            )}
+                        </td>
+                    ))}
+                </tr>
+            </tbody>
+        </table>
+    );
+};
+
+export default ElementDetailTable;
--- src/app/components/pages/ElementDetailPage.tsx
+++ src/app/components/pages/ElementDetailPage.tsx
@@ -1,5 +1,36 @@
-const ElementDetailPage = () => {
-    return <>ElementDetailPage</>;
+import { Navigate } from 'react-router-dom';
+import { getCategoryElementsList } from '../../utils/getCategoryElementsList';
+import ElementDetailTable from '../common/tables/ElementDetailTable';
+import type { Category } from '../../models/models';
+
+interface ElementDetailPageProps {
+    category: Category;
+    id: string;
+}
+
+const ElementDetailPage = ({
+    category,
+    id: elementId,
+}: ElementDetailPageProps) => {
+    const сategoryElementsList = getCategoryElementsList(category);
+    const element = сategoryElementsList[category]!.find(
+        item => item.id.toString() === elementId.toString()
+    );
+
+    if (!element) {
+        return <Navigate to="/" />;
+    }
+
+    return (
+        <div className="page">
+            <div className="container">
+                <div className="page__inner">
+                    <h1 className="page__title">{element.name}</h1>
+                    <ElementDetailTable element={element} />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ElementDetailPage;
