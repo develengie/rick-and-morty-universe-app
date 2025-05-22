b867281cfbd10ee240b40cfbda3a1bed6ccb3c14
Alexey <alexeyushakov1997@yandex.ru>
2025-05-22 13:15:47 +0300

    feat: created navigation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,21 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
+import Navigation from './components/ui/Navigation/Navigation';
 import MainPage from './components/pages/MainPage';
 import CategoriesLayout from './layouts/CategoriesLayout';
 
 const App = () => {
     return (
-        <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route
-                path="/categories/:category/:id?"
-                element={<CategoriesLayout />}
-            />
-            <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+        <>
+            <Navigation />
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route
+                    path="/categories/:category/:id?"
+                    element={<CategoriesLayout />}
+                />
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </>
     );
 };
 
