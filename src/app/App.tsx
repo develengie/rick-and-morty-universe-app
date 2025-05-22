46459ca9d66e5d7d4ee97e26f915201d30ec9c9e
Alexey <alexeyushakov1997@yandex.ru>
2025-05-22 12:41:09 +0300

    feat: created routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,18 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import MainPage from './components/pages/MainPage';
+import CategoriesLayout from './layouts/CategoriesLayout';
+
 const App = () => {
-    return <></>;
+    return (
+        <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route
+                path="/categories/:category/:id?"
+                element={<CategoriesLayout />}
+            />
+            <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+    );
 };
 
 export default App;
