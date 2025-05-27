5d1c9ff471632649af27d053dd4717ace53198b9
Alexey <alexeyushakov1997@yandex.ru>
2025-05-27 12:46:09 +0300

    chore: changed routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,20 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import Navigation from './components/ui/Navigation/Navigation';
+import MainLayout from './layouts/MainLayout';
 import MainPage from './components/pages/MainPage';
 import CategoriesLayout from './layouts/CategoriesLayout';
 
 const App = () => {
     return (
-        <>
-            <Navigation />
-            <Routes>
+        <Routes>
+            <Route element={<MainLayout />}>
                 <Route path="/" element={<MainPage />} />
                 <Route
                     path="/categories/:category/:id?"
                     element={<CategoriesLayout />}
                 />
                 <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
-        </>
+            </Route>
+        </Routes>
     );
 };
 
