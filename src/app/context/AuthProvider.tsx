616d42d9f87b3922629577c2b139ca15e0689336
Alexey <alexeyushakov1997@yandex.ru>
2025-05-27 21:04:17 +0300

    feat: added localStorage methods

--- src/app/context/AuthProvider.tsx
+++ src/app/context/AuthProvider.tsx
@@ -18,15 +18,19 @@ export const useAuth = () => {
 };
 
 const AuthProvider = ({ children }: AuthProviderProps) => {
-    const [user, setUser] = useState<User>({} as User);
+    const [user, setUser] = useState<User>(
+        () => JSON.parse(localStorage.getItem('user')!) ?? {}
+    );
 
     const signin = (user: User, callback: () => void) => {
         setUser(user);
+        localStorage.setItem('user', JSON.stringify(user));
         callback();
     };
 
     const signout = (callback: () => void) => {
         setUser({} as User);
+        localStorage.removeItem('user');
         callback();
     };
 
