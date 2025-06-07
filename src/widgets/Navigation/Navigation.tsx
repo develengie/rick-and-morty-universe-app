90f2727c2013b4110b62b6de4adc2fe8554cf332
Alexey Ushakov <develengie@gmail.com>
2025-06-07 17:36:42 +0300

    feat: added Breadcrumbs component

--- src/widgets/Navigation/Navigation.tsx
+++ src/widgets/Navigation/Navigation.tsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import { Breadcrumbs } from "@mui/material";
 import { useAuth } from "../../app/providers/AuthProvider";
 import internalPaths from "../../app/internalPaths";
 import categories from "../../app/categories";
@@ -7,57 +8,32 @@ import "./Navigation.scss";
 const Navigation = () => {
     const auth = useAuth();
 
+    const authNavLinks = [
+        { category: categories.characters.name, text: "Characters" },
+        { category: categories.location.name, text: "Location" },
+        { category: categories.episode.name, text: "Episode" },
+    ];
+
     return (
-        <nav className="nav">
-            <ul className="nav__list">
-                <li className="nav__list-item">
-                    <NavLink className="nav__link" to={internalPaths.main}>
-                        Main
+        <Breadcrumbs className="nav" aria-label="breadcrumb">
+            <NavLink className="nav__link" to={internalPaths.main}>
+                Main
+            </NavLink>
+            {!auth.user.email && (
+                <NavLink className="nav__link" to={internalPaths.login}>
+                    Login
+                </NavLink>
+            )}
+            {auth.user.email &&
+                authNavLinks.map((navLink) => (
+                    <NavLink
+                        className="nav__link"
+                        to={internalPaths.category(navLink.category)}
+                    >
+                        {navLink.text}
                     </NavLink>
-                </li>
-                {!auth.user.email && (
-                    <li className="nav__list-item">
-                        <NavLink className="nav__link" to={internalPaths.login}>
-                            Login
-                        </NavLink>
-                    </li>
-                )}
-                {auth.user.email && (
-                    <>
-                        <li className="nav__list-item">
-                            <NavLink
-                                className="nav__link"
-                                to={internalPaths.category(
-                                    categories.characters.name
-                                )}
-                            >
-                                Characters
-                            </NavLink>
-                        </li>
-                        <li className="nav__list-item">
-                            <NavLink
-                                className="nav__link"
-                                to={internalPaths.category(
-                                    categories.location.name
-                                )}
-                            >
-                                Location
-                            </NavLink>
-                        </li>
-                        <li className="nav__list-item">
-                            <NavLink
-                                className="nav__link"
-                                to={internalPaths.category(
-                                    categories.episode.name
-                                )}
-                            >
-                                Episode
-                            </NavLink>
-                        </li>
-                    </>
-                )}
-            </ul>
-        </nav>
+                ))}
+        </Breadcrumbs>
     );
 };
 
