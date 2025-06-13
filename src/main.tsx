dde47a074e5dd95295fda95fcf05c5e5fc7fa7c4
Alexey Ushakov <develengie@gmail.com>
2025-06-13 17:25:41 +0300

    chore: registered Service Worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.scss';
-import App from './app/App.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.scss";
+import App from "./app/App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter>
             <App />
         </BrowserRouter>
     </StrictMode>
 );
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+        .register("/sw.ts")
+        .then((reg) => console.log("Service Worker registered.", reg))
+        .catch((error) => console.log("Service Worker not registered.", error));
+}
